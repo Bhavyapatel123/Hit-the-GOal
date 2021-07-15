55c40aa0198e2c91871a5dd71e0e2d640970f78b
Bhavyapatel123 <83211071+Bhavyapatel123@users.noreply.github.com>
2021-07-15 11:33:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,158 @@
+
+
+var canvas=new fabric.Canvas("myCanvas");
+block_width=30;
+block_height=30;
+player_x=10;
+player_y=10;
+var player_object="";
+
+function player_update(){
+    fabric.Image.fromURL("player.png",function(Img){
+        player_object=Img;
+        player_object.scaleToWidth(150);
+        player_object.scaleToHeight(140);
+        player_object.set({
+            top:player_y,
+            left:player_x
+        });
+        canvas.add(player_object);
+    });
+}
+
+var block_object="";
+
+function new_image(get_image){
+    fabric.Image.fromURL(get_image,function(Img){
+        block_object=Img;
+        block_object.scaleToWidth(block_width);
+        block_object.scaleToHeight(block_height);
+        block_object.set({
+            top:player_y,
+            left:player_x
+        });
+        canvas.add(block_object);
+    });
+}
+
+window.addEventListener("keydown",my_keydown);
+function my_keydown(e){
+    keypress=e.keyCode;
+    console.log(keypress);
+    if(e.shiftKey&& keypress=="80"){
+        console.log("pandshiftkeytogether");
+        block_width=block_width+10;
+        block_height=block_height+10;
+        document.getElementById("current_width").innerHTML=block_width;
+        document.getElementById("current_height").innerHTML=block_height;
+    }
+
+    if(e.shiftKey&& keypress=="77"){
+        console.log("mandshiftkeytogether");
+        block_width=block_width-10;
+        block_height=block_height-10;
+        document.getElementById("current_width").innerHTML=block_width;
+        document.getElementById("current_height").innerHTML=block_height;
+    }
+
+    if(keypress=="38"){
+    up();
+    console.log("up");
+    }
+
+    if(keypress=="40"){
+        down();
+        console.log("down");
+        }
+
+        if(keypress=="37"){
+            left();
+            console.log("left");
+            }
+
+            if(keypress=="39"){
+                right();
+                console.log("right");
+                }
+                
+                if(keypress=="87"){
+                    new_image("wall.jpg");
+                    console.log("w");
+                }
+
+                if(keypress=="71"){
+                    new_image("ground.png");
+                    console.log("g");
+                }
+
+                if(keypress=="76"){
+                    new_image("light_green.png");
+                    console.log("l");
+                }
+
+                if(keypress=="84"){
+                    new_image("trunk.jpg");
+                    console.log("t");
+                }
+
+                if(keypress=="82"){
+                    new_image("roof.jpg");
+                    console.log("r");
+                }
+
+                if(keypress=="89"){
+                    new_image("yellow_wall.png");
+                    console.log("y");
+                }
+
+                if(keypress=="68"){
+                    new_image("dark_green.png");
+                    console.log("d");
+                }
+
+                if(keypress=="85"){
+                    new_image("unique.png");
+                    console.log("u");
+                }
+
+                if(keypress=="67"){
+                    new_image("cloud.jpg");
+                    console.log("c");
+                }
+}
+
+function up(){
+    if(player_y>=0){
+        player_y=player_y-block_height;
+        console.log(player_y);
+canvas.remove(player_object);
+player_update();
+    }
+}
+
+function down(){
+    if(player_y<=500){
+        player_y=player_y+block_height;
+        console.log(player_y);
+canvas.remove(player_object);
+player_update();
+    }
+}
+
+function left(){
+    if(player_x>=0){
+        player_x=player_x-block_width;
+        console.log(player_x);
+canvas.remove(player_object);
+player_update();
+    }
+}
+
+function right(){
+    if(player_x<=750){
+        player_x=player_x+block_width;
+        console.log(player_x);
+canvas.remove(player_object);
+player_update();
+    }
+}
